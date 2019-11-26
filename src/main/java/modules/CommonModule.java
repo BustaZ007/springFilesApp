1ea28a452c7778c4d7ad638183b17fd3c73d5292
Pavel <pavel.zaborin@inbox.ru>
2019-11-26 19:46:44 +0500

    Start

---
+++ src/main/java/modules/CommonModule.java
@@ -0,0 +1,8 @@
+package modules;
+
+public interface CommonModule {
+
+    public boolean chechExtention(String extention);
+
+    public void chooseFunction(String name);
+}
---
+++ src/main/java/modules/MusicModule.java
@@ -0,0 +1,14 @@
+package modules;
+
+import com.mpatric.mp3agic.Mp3File;
+
+public class MusicModule implements CommonModule {
+
+    public boolean chechExtention(String extention) {
+        return false;
+    }
+
+    public void chooseFunction(String name) {
+
+    }
+}
